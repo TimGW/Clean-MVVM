7b0d92b52fab991972fee33fc402759021ef0f13
Tim Gortworst <tim.gortworst@gmail.com>
2021-06-09 20:31:43 +0200

    update MovieListAdapter to use a layout that's more friendly for horizontal nested orientation

--- presentation/src/main/java/com/timgortworst/cleanarchitecture/presentation/features/movie/list/adapter/MovieListAdapter.kt
+++ presentation/src/main/java/com/timgortworst/cleanarchitecture/presentation/features/movie/list/adapter/MovieListAdapter.kt
@@ -1,4 +1,4 @@
-package com.timgortworst.cleanarchitecture.presentation.features.movie.list
+package com.timgortworst.cleanarchitecture.presentation.features.movie.list.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -8,14 +8,15 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.timgortworst.cleanarchitecture.domain.model.movie.Movie
 import com.timgortworst.cleanarchitecture.presentation.R
-import com.timgortworst.cleanarchitecture.presentation.databinding.MovieListItemBinding
+import com.timgortworst.cleanarchitecture.presentation.databinding.MovieListItemNestedBinding
 
 class MovieListAdapter(
-    private var clickListener: ((Movie, ImageView) -> Unit)? = null
+    private val movieList: List<Movie>
 ) : RecyclerView.Adapter<MovieListAdapter.ViewHolder>() {
-    private val movieList = mutableListOf<Movie>()
+    var clickListener: ((Movie, ImageView) -> Unit)? = null
 
     init {
+        stateRestorationPolicy = StateRestorationPolicy.PREVENT_WHEN_EMPTY
         setHasStableIds(true)
     }
 
@@ -23,10 +24,14 @@ class MovieListAdapter(
         return movieList[position].id.toLong()
     }
 
-    override fun getItemCount() = movieList.size
+    override fun getItemViewType(position: Int): Int {
+        return R.layout.movie_list_item_nested
+    }
+
+    override fun getItemCount() = if(movieList.isEmpty()) 0 else movieList.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(
-        MovieListItemBinding.inflate(
+        MovieListItemNestedBinding.inflate(
             LayoutInflater.from(parent.context),
             parent,
             false
@@ -35,15 +40,15 @@ class MovieListAdapter(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val movie = movieList[position]
-        holder.bindMovie(movie)
+        holder.bind(movie)
     }
 
     inner class ViewHolder(
-        binding: MovieListItemBinding
+        binding: MovieListItemNestedBinding
     ) : RecyclerView.ViewHolder(binding.root) {
         private val moviePoster: ImageView = binding.moveListItemImage
 
-        fun bindMovie(movie: Movie) {
+        fun bind(movie: Movie) {
             moviePoster.apply {
                 Glide.with(context)
                     .load(movie.lowResImage)
@@ -57,11 +62,5 @@ class MovieListAdapter(
                 clickListener?.invoke(movieList[bindingAdapterPosition], moviePoster)
             }
         }
-    }
-
-    fun addMoviesToList(movieList: MutableList<Movie>) {
-        this.movieList.clear()
-        this.movieList.addAll(movieList)
-        notifyDataSetChanged()
     }
 }
