8bc936f996c4e24aa5775e49a52f8f243ef8c0db
Tim Gortworst <tim.gortworst@gmail.com>
2021-06-09 20:21:23 +0200

    update horizontal nested recyclerview

--- presentation/src/main/java/com/timgortworst/cleanarchitecture/presentation/features/movie/list/adapter/NestedRecyclerAdapter.kt
+++ presentation/src/main/java/com/timgortworst/cleanarchitecture/presentation/features/movie/list/adapter/NestedRecyclerAdapter.kt
@@ -6,14 +6,13 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.HORIZONTAL
-import com.timgortworst.cleanarchitecture.domain.model.movie.Movie
 import com.timgortworst.cleanarchitecture.presentation.R
 import com.timgortworst.cleanarchitecture.presentation.databinding.MovieListWrapperBinding
 import com.timgortworst.cleanarchitecture.presentation.features.movie.list.decoration.HorizontalListMarginDecoration
 
-class MovieHorizontalListAdapter(
-    private val movies: MutableList<Movie> = mutableListOf()
-) : RecyclerView.Adapter<MovieHorizontalListAdapter.ViewHolder>() {
+class NestedRecyclerAdapter<T : RecyclerView.Adapter<*>>(
+    private val listItemAdapter: T,
+) : RecyclerView.Adapter<NestedRecyclerAdapter<T>.ViewHolder>() {
     private val scrollStates: MutableMap<String, Parcelable?> = mutableMapOf()
     private val viewPool = RecyclerView.RecycledViewPool()
 
@@ -22,7 +21,7 @@ class MovieHorizontalListAdapter(
     }
 
     private fun getSectionID(position: Int): String {
-        return movies.getOrNull(position)?.id?.toString().orEmpty()
+        return listItemAdapter.getItemId(position).toString()
     }
 
     override fun onViewRecycled(holder: ViewHolder) {
@@ -32,43 +31,33 @@ class MovieHorizontalListAdapter(
         scrollStates[key] = holder.rv.layoutManager?.onSaveInstanceState()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = MovieListWrapperBinding.inflate(
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(
+        MovieListWrapperBinding.inflate(
             LayoutInflater.from(parent.context),
             parent,
             false
         )
+    )
 
-        return ViewHolder(binding)
-    }
-
-    override fun getItemCount(): Int = if (movies.isEmpty()) 0 else 1
+    override fun getItemCount(): Int = if (listItemAdapter.itemCount > 0) 1 else 0
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(movies)
-    }
-
-    fun addMoviesSection(movieList: List<Movie>) {
-        movies.clear()
-        movies.addAll(movieList)
-        notifyDataSetChanged() // FIXME
-//        notifyItemRangeChanged(0, movieList.size, movieList)
-
+        holder.bind(listItemAdapter)
     }
 
     inner class ViewHolder(
         binding: MovieListWrapperBinding
     ) : RecyclerView.ViewHolder(binding.root) {
         val rv = binding.recyclerView
+        private val lm = LinearLayoutManager(rv.context, HORIZONTAL, false).apply {
+            initialPrefetchItemCount = 4
+        }
 
-        fun bind(movies: List<Movie>) {
-            val lm = LinearLayoutManager(rv.context, HORIZONTAL, false)
-            lm.initialPrefetchItemCount = 4
-
+        fun bind(itemAdapter: T) {
             rv.apply {
                 setRecycledViewPool(viewPool)
                 layoutManager = lm
-                adapter = MovieListAdapter(movies.toMutableList())
+                adapter = itemAdapter
             }
 
             restoreState()
