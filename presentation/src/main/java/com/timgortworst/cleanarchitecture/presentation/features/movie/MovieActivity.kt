1c89c2433d678bd6b45476cf735742f34d7086e3
Tim Gortworst <tim.gortworst@gmail.com>
2021-06-10 18:35:43 +0200

    delete code from MovieActivity.kt

--- presentation/src/main/java/com/timgortworst/cleanarchitecture/presentation/features/movie/MovieActivity.kt
+++ presentation/src/main/java/com/timgortworst/cleanarchitecture/presentation/features/movie/MovieActivity.kt
@@ -4,17 +4,11 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import com.timgortworst.cleanarchitecture.presentation.R
 import com.timgortworst.cleanarchitecture.presentation.databinding.ActivityMovieBinding
-import com.timgortworst.cleanarchitecture.presentation.extension.setTranslucentStatus
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MovieActivity : AppCompatActivity() {
-    private lateinit var navController: NavController
-    private lateinit var binding: ActivityMovieBinding
 
     companion object {
         fun intentBuilder(context: Context): Intent {
@@ -24,17 +18,6 @@ class MovieActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMovieBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
-        navController = (navHostFragment as NavHostFragment).navController
-
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            when (destination.id) {
-                R.id.fragmentMovieList -> setTranslucentStatus(false)
-                R.id.fragmentMovieDetails -> setTranslucentStatus(true)
-            }
-        }
+        setContentView(ActivityMovieBinding.inflate(layoutInflater).root)
     }
 }
