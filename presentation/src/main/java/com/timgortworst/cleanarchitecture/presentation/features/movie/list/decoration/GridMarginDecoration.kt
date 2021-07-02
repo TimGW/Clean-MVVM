e2a7cb8d9792d642000b72006ab1a4bc1ea907e6
Tim Gortworst <tim.gortworst@gmail.com>
2021-07-02 09:33:14 +0200

    update way to find the current adapter for a position

--- presentation/src/main/java/com/timgortworst/cleanarchitecture/presentation/features/movie/list/decoration/GridMarginDecoration.kt
+++ presentation/src/main/java/com/timgortworst/cleanarchitecture/presentation/features/movie/list/decoration/GridMarginDecoration.kt
@@ -2,7 +2,6 @@ package com.timgortworst.cleanarchitecture.presentation.features.movie.list.deco
 
 import android.graphics.Rect
 import android.view.View
-import androidx.recyclerview.widget.ConcatAdapter
 import androidx.recyclerview.widget.RecyclerView
 
 class GridMarginDecoration : RecyclerView.ItemDecoration() {
@@ -14,8 +13,7 @@ class GridMarginDecoration : RecyclerView.ItemDecoration() {
         state: RecyclerView.State
     ) {
         val position: Int = parent.getChildAdapterPosition(view)
-        val adapter = (parent.adapter as? ConcatAdapter) ?: return
-        val decoration = adapter.getDecoration(parent, position) ?: return
+        val decoration = getDecoration(parent, position) ?: return
 
         outRect.left = decoration.left
         outRect.top = decoration.top
@@ -23,20 +21,15 @@ class GridMarginDecoration : RecyclerView.ItemDecoration() {
         outRect.bottom = decoration.bottom
     }
 
-    private fun ConcatAdapter.getDecoration(parent: RecyclerView, adapterPosition: Int): Rect? {
-        val currentItemType = getItemViewType(adapterPosition)
-        val adapter = adapters.find { it.getItemViewType(adapterPosition) == currentItemType }
-        val relativePosition = parent.findViewHolderForAdapterPosition(adapterPosition)
-            ?.bindingAdapterPosition ?: return null
+    private fun getDecoration(parent: RecyclerView, adapterPosition: Int): Rect? {
+        val adapter = parent.findViewHolderForLayoutPosition(adapterPosition)?.bindingAdapter
+        val relativePosition = parent.findViewHolderForLayoutPosition(adapterPosition)
+            ?.bindingAdapterPosition ?: adapterPosition
 
         return (adapter as? AdapterDecoration)?.getItemDecoration(
             parent.resources,
             adapterPosition,
             relativePosition
         )
-
-        // this causes recycling issues
-//        val adapter = recyclerView.findViewHolderForAdapterPosition(position)?.bindingAdapter
-//        return (adapter as? AdapterDecoration)?.getItemDecoration(recyclerView.resources, position)
     }
 }
